1504a9f82cf0a45fe3c30aab32ca286f4e78a89b
blueslimee <lyricalsoul@pm.me>
2021-04-06 23:03:34 -0400

    bruh

---
+++ api/collage.js
@@ -0,0 +1,14 @@
+const { createCanvas, registerFont } = require('node-canvas')
+registerFont('../public/fonts/montserrat/Montserrat-Bold.ttf', { family: 'Montserrat', weight: 'bold' })
+module.exports = async (req, res) => {
+  const { name = "jesus" } = req.query
+  res.setHeader('content-type', 'application/png')
+
+  const canvas = createCanvas(1000, 300)
+  const ctx = canvas.getContext('2d')
+  ctx.font = 'bold 12px Montserrat'
+  
+  ctx.fillText(name, 500, 150)
+  res.status(200).send(await canvas.toBuffer('application/png'))
+}
+

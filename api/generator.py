20becb3a0d5c830f7d02f23e685798713a604b44
blueslimee <lyricalsoul@pm.me>
2021-04-07 14:20:45 -0400

    bruh

--- api/generator.py
+++ api/generator.py
@@ -15,8 +15,7 @@ def gen_collage(w, h):
 @app.route('/')
 def root():
     if request.args.get('r') == 'collage':
-        return send_file(gen_collage(request.args.get('w'), req\
-    uest.args.get('h'), mimetype='image/jpeg'))
+        return send_file(gen_collage(request.args.get('w'), request.args.get('h')), mimetype='image/jpeg')
 
 def to_jpeg(img):
     io = BytesIO()
